6e341084ea2a525630f16835b8da1349b50fe991
Sahil Dangi <154420933+gsahildangi@users.noreply.github.com>
2024-08-23 14:38:56 +0000

    full

--- 10todoContextLocal/src/contexts/TodoContext.js
+++ 10todoContextLocal/src/contexts/TodoContext.js
@@ -11,7 +11,7 @@ export const TodoContext = createContext({
     addTodo:(todo)=>{},
     deleteTodo:(id)=>{},
     updateTodo:(id,todo)=>{},
-    toggleCompleted:(id)=>{}
+    toggleComplete:(id)=>{}
 })
 
 export const useTodo = () => {
